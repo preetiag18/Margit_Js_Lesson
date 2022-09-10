a6555decdeb41fffb3792e53d2b7fb92deeb1674
preetiag18 <preetiag18@gmail.com>
2022-09-10 12:28:00 +0300

    css changes in gd colors

--- week5/gradientColor/script.js
+++ week5/gradientColor/script.js
@@ -9,13 +9,19 @@ const body = document.getElementById("gradient");
 //Create new function for color pick
 function setGradient(event) {
 	const selectedArrow = document.querySelector('input[name="arrows"]:checked').value;
-	body.style.background = "linear-gradient("+ selectedArrow+ "deg, " + color1.value + ", " 
-	+ color2.value +")";
-	h3.textContent = body.style.background + ";";
+	body.style.background = 
+	"linear-gradient("
+	+ selectedArrow 
+	+ ","
+	+ color1.value 
+	+ ", "
+	+ color2.value 
+	+ ")";
+	h3.textContent = body.style.background + "";
 }
 
-
 //Pick and set Bkground colors 
+
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
 
